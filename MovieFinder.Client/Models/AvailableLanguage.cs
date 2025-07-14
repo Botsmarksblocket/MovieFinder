adf30584541414226f06af8fb1665f6123c31b9f
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-07-14 14:58:40 +0300

    Started working on adding info about language, revenue and rating.

---
+++ MovieFinder.Client/Models/AvailableLanguage.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MovieFinder.Client.Models
+{
+    public class AvailableLanguage
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
