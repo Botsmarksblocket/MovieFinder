7cb8e98e92a8c00e4956c678c8fdfc19fba3849d
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-07-16 13:07:12 +0300

    Created ActorPage. Made it possible to direct to it via actor images. Created ActorDetail class and new service to retrieve actor details.

---
+++ MovieFinder.Client/Models/ActorDetail.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MovieFinder.Client.Models
+{
+    public class ActorDetail
+    {
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("biography")]
+        public string Biography { get; set; }
+
+        [JsonPropertyName("birthday")]
+        public string Birthday { get; set; }
+
+        [JsonPropertyName("place_of_birth")]
+        public string PlaceOfBirth { get; set; }
+
+        [JsonPropertyName("profile_path")]
+        public string ProfilePicture { get; set; }
+    }
+}
