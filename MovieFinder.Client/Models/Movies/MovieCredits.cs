7ab0be9760e44bff532504d7b01d23fd226082f9
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-07-20 15:14:51 +0300

    Finished the pagination for the actors images and related movies.

--- MovieFinder.Client/Models/Movies/MovieCredits.cs
+++ MovieFinder.Client/Models/Movies/MovieCredits.cs
@@ -5,22 +5,6 @@ namespace MovieFinder.Client.Models.Movies
     public class MovieCredits
     {
         [JsonPropertyName("cast")]
-        public List<MovieCreditItem> Cast { get; set; }
-    }
-
-    public class MovieCreditItem
-    {
-        public int Id { get; set; }
-
-        [JsonPropertyName("poster_path")]
-        public string PosterPath { get; set; }
-
-        [JsonPropertyName("title")]
-        public string Title { get; set; }
-
-        [JsonPropertyName("release_date")]
-        public string ReleaseDate { get; set; }
-        [JsonPropertyName("vote_average")]
-        public double Rating { get; set; }
+        public List<Movie> Cast { get; set; }
     }
 }
